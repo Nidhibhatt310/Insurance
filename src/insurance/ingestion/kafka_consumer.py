ea474cd46cc4924ec4bd8bcde42c638ef4c6f3d0
NidhiBhatt <nidhimbhatt310@gmail.com>
2026-04-23 21:32:56 +0530

    changes for write stream

--- src/insurance/ingestion/kafka_consumer.py
+++ src/insurance/ingestion/kafka_consumer.py
@@ -1,11 +1,9 @@
 import sys
+
 base_location = sys.argv[1]
 
+
 class consumeKafkaData:
-<<<<<<< HEAD
-    
-=======
->>>>>>> 8a41b1d7a0a0deb5a8ea424a47919dc4dbeb7c0e
     def __init__(self, topic):
         self.topic = topic
 
@@ -16,10 +14,9 @@ class consumeKafkaData:
         bootstrap_server = spark.conf.get("bootstrap_server")
         kafka_key = spark.conf.get("kafka_key")
         kafka_secret = spark.conf.get("kafka_secret")
-        JAAS_MODULE = 'org.apache.kafka.common.security.plain.PlainLoginModule'
+        JAAS_MODULE = "org.apache.kafka.common.security.plain.PlainLoginModule"
         df = (
-            spark.readStream
-            .format("kafka")
+            spark.readStream.format("kafka")
             .option("kafka.bootstrap.servers", bootstrap_server)
             .option("kafka.security.protocol", "SASL_SSL")
             .option("kafka.sasl.mechanism", "PLAIN")
@@ -27,30 +24,24 @@ class consumeKafkaData:
                 "kafka.sasl.jaas.config", f'{JAAS_MODULE} required username="{kafka_key}" password="{kafka_secret}";'
             )
             .option("subscribe", self.topic)
-            .option("startingOffsets", "earliest")
+            .option("startingOffsets", "latest")
             .load()
         )
         return df
 
     def write_to_bronze(self, df):
-        return (
-            df.writeStream
-            .format('delta')
-            .option('checkpointLocation',f'{base_location}/bronze/{self.topic}')
+        query = (
+            df.writeStream.format("delta")
+            .option("checkpointLocation", f"{base_location}/bronze/{self.topic}")
             .queryName("kafka_bronze_stream")
-            .outputMode('append')
+            .outputMode("append")
             .trigger(availableNow=True)
-            .toTable(f'insurance_dev.bronze.{self.topic}')
+            .toTable(f"insurance_dev.bronze.{self.topic}")
         )
-
+        query.awaitTermination()
 
 
 claims = consumeKafkaData(topic="claims")
-<<<<<<< HEAD
 read_claims_df = claims.read_from_kafka()
 
 claims.write_to_bronze(read_claims_df)
-=======
-read_claims = claims.read_from_kafka()
-read_claims.show()
->>>>>>> 8a41b1d7a0a0deb5a8ea424a47919dc4dbeb7c0e
